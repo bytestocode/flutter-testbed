a630cce1cdb8d6843768ace5b6f7157d502accbe
Mies <nature9th@gmail.com>
2022-08-25 21:32:00 +0900

    study: margin과 padding의 차이점은?

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_testbed/factory_container.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Testbed',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.indigo),
       home: const TestBed(),
     );
   }
 }
 
 class TestBed extends StatelessWidget {
-  const TestBed({Key? key}) : super(key: key);
+  const TestBed({super.key});
 
   @override
   Widget build(BuildContext context) {
+    /// margin, padding 차이점 탐구
     return SafeArea(
       child: Scaffold(
-        body: Column(
-  
-          children: [
-            FactoryContainer.onlyText('yahoo'),
-            FactoryContainer.onlyText('yahoo'),
-            FactoryContainer.onlyText('yahoo'),
-            FactoryContainer.withColor('yahoo', color: Colors.blue),
-            FactoryContainer.withColor('yahoo', color: Colors.yellow),
-            FactoryContainer.withColor('yahoo', color: Colors.green),
-          ],
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                  color: Colors.red,
+                ),
+                child: Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(50.0),
+                    color: Colors.green,
+                    height: 200,
+                    width: 200,
+                    child: Container(
+                      color: Colors.amber,
+                      height: 10,
+                      width: 10,
+                      child: const Text('1번'),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Container(
+                  margin: const EdgeInsets.all(50.0),
+                  color: Colors.pink,
+                  height: 200,
+                  width: 200,
+                  child: Container(
+                    color: Colors.amber,
+                    height: 10,
+                    width: 10,
+                    child: const Text('2번'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
