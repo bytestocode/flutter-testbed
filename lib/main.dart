6e93a9be98f266d503098c4f731aac12f291956f
Mies <nature9th@gmail.com>
2022-10-05 21:37:39 +0900

    study: SliverFillRemaining 적용해보기

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -7,92 +5,40 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const String _title = 'SliverFillRemaining';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Testbed',
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: const TestBed(),
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatelessWidget(),
+      ),
     );
   }
 }
 
-class TestBed extends StatefulWidget {
-  const TestBed({super.key});
-
-  @override
-  State<TestBed> createState() => _TestBedState();
-}
-
-class _TestBedState extends State<TestBed> {
-  void _asyncFunction() async {
-    log('await 전'); // (1) 번째
-    await Future.delayed(const Duration(milliseconds: 300));
-    log('await 후'); // (3) 번째
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _asyncFunction();
-    log('비동기 함수보다 먼저 실행될까?'); // (2) 번째
-  }
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SizedBox(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: 300,
-                width: 300,
-                decoration: const BoxDecoration(
-                  color: Colors.red,
-                ),
-                child: Center(
-                  child: Container(
-                    padding: const EdgeInsets.all(50.0),
-                    color: Colors.green,
-                    height: 200,
-                    width: 200,
-                    child: Container(
-                      color: Colors.amber,
-                      height: 10,
-                      width: 10,
-                      child: const Text('1번'),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              Container(
-                height: 300,
-                width: 300,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Container(
-                  margin: const EdgeInsets.all(50.0),
-                  color: Colors.pink,
-                  height: 200,
-                  width: 200,
-                  child: Container(
-                    color: Colors.amber,
-                    height: 10,
-                    width: 10,
-                    child: const Text('2번'),
-                  ),
-                ),
-              ),
-            ],
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Container(
+            color: Colors.amber[300],
+            height: 550.0,
           ),
         ),
-      ),
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Container(
+            color: Colors.redAccent,
+          ),
+        ),
+      ],
     );
   }
 }
