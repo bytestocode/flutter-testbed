a9c4801a1195e2b9842f205c152f89f2bda8ca22
Mies <nature9th@gmail.com>
2023-06-25 11:13:04 +0900

    test: (S.O.) 블러효과 일부만 적용하는 방법

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -22,30 +24,74 @@ class TestScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Center(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 40,
-                  crossAxisSpacing: 2.5,
+      body: Container(
+        height: 400,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://picsum.photos/200'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Center(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 40,
+                    crossAxisSpacing: 2.5,
+                  ),
+                  scrollDirection: Axis.vertical,
+                  itemCount: 19,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return const Text(
+                      '_',
+                    );
+                  },
                 ),
-                scrollDirection: Axis.vertical,
-                itemCount: 19,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return const Text(
-                    '_',
-                  );
-                },
               ),
-            ),
-          ],
+              loginContainer(),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Widget loginContainer() {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.white70,
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: 5.0,
+          sigmaY: 5.0,
+        ),
+        child: getLoginContainer(),
+      ),
+    ),
+  );
+}
+
+Widget getLoginContainer() {
+  return Column(
+    children: [
+      Text('df'),
+      const SizedBox(
+        height: 10.0,
+      ),
+      Text('df'),
+      const SizedBox(
+        height: 10.0,
+      ),
+    ],
+  );
 }
