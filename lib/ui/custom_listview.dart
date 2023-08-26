84da8e7d25a75012a47ebb0d398c103a36e8121f
Mies <nature9th@gmail.com>
2023-08-26 21:47:45 +0900

    fix: resolve conflicts

--- lib/ui/custom_listview.dart
+++ lib/ui/custom_listview.dart
@@ -37,102 +37,107 @@ class UserPost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        // primary: false,
-        shrinkWrap: true,
-        physics: const BouncingScrollPhysics(),
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Colors.grey.shade200),
-                  ),
-                  title: const Text("Ava Sadie"),
-                  subtitle: Text(
-                    "Sylhet, Bangladesh",
-                    style: Theme.of(context).textTheme.labelSmall,
-                  ),
-                  trailing: IconButton(
-                    splashRadius: 20.0,
-                    onPressed: () {},
-                    icon: const Icon(Icons.more_horiz_outlined),
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 250,
-                  margin: const EdgeInsets.only(top: 5.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade200,
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                ),
-                const SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+    return Column(
+      children: [
+        ListView.builder(
+            // primary: false,
+            shrinkWrap: true,
+            physics: const BouncingScrollPhysics(),
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 10.0),
+                child: Column(
                   children: [
+                    ListTile(
+                      leading: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey.shade200),
+                      ),
+                      title: const Text("Ava Sadie"),
+                      subtitle: Text(
+                        "Sylhet, Bangladesh",
+                        style: Theme.of(context).textTheme.labelSmall,
+                      ),
+                      trailing: IconButton(
+                        splashRadius: 20.0,
+                        onPressed: () {},
+                        icon: const Icon(Icons.more_horiz_outlined),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 250,
+                      margin: const EdgeInsets.only(top: 5.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        IconButton(
-                          splashRadius: 20.0,
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.favorite_outline,
-                          ),
+                        Row(
+                          children: [
+                            IconButton(
+                              splashRadius: 20.0,
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.favorite_outline,
+                              ),
+                            ),
+                            Text(
+                              "6.2k",
+                              style: Theme.of(context).textTheme.displaySmall,
+                            ),
+                          ],
                         ),
-                        Text(
-                          "6.2k",
-                          style: Theme.of(context).textTheme.displaySmall,
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                              splashRadius: 20.0,
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.messenger_outline,
+                              ),
+                            ),
+                            Text(
+                              "2.1k",
+                              style: Theme.of(context).textTheme.displaySmall,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                              splashRadius: 20.0,
+                              onPressed: () {},
+                              icon: const Icon(Icons.bookmark_border_outlined),
+                            ),
+                            Text(
+                              "3.5k",
+                              style: Theme.of(context).textTheme.displaySmall,
+                            ),
+                          ],
                         ),
                       ],
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Row(
-                      children: [
-                        IconButton(
-                          splashRadius: 20.0,
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.messenger_outline,
-                          ),
-                        ),
-                        Text(
-                          "2.1k",
-                          style: Theme.of(context).textTheme.displaySmall,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Row(
-                      children: [
-                        IconButton(
-                          splashRadius: 20.0,
-                          onPressed: () {},
-                          icon: const Icon(Icons.bookmark_border_outlined),
-                        ),
-                        Text(
-                          "3.5k",
-                          style: Theme.of(context).textTheme.displaySmall,
-                        ),
-                      ],
-                    ),
+                    )
                   ],
-                )
-              ],
-            ),
-          );
-        });
+                ),
+              );
+            }),
+      ],
+    );
   }
 }
